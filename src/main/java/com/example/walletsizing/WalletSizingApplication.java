18704d9f7a192eb15849af99f48dc2ee8724fe4c
Geoffrey Kimani <geoffrey.kimani@KEMDGF231B.local>
2026-02-16 15:35:48 +0300

    Sprint 0: Infrastructure and Wallet Entity

---
+++ src/main/java/com/example/walletsizing/WalletSizingApplication.java
@@ -0,0 +1,13 @@
+package com.example.walletsizing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletSizingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WalletSizingApplication.class, args);
+    }
+
+}
